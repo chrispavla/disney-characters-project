ee09b27e1bb8c87b8589b4e3f3f65b1b7f507553
Wendy You <wyou130@gmail.com>
2022-08-20 13:19:42 -0400

    Set up components in App with Switch Route, added NavLinks to NavBar, added very simple styling to active class

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,23 +1,29 @@
-import logo from '../logo.svg';
 import '../App.css';
+import { Route, Switch } from "react-router-dom"
+import NavBar from "./NavBar"
+import Home from './Home';
+import CharactersList from './CharactersList';
+import FavoriteCharacters from './FavoriteCharacters';
+import NewCharacterForm from './NewCharacterForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/all">
+          <CharactersList />
+        </Route>
+        <Route path="/favorites">
+          <FavoriteCharacters />
+        </Route>
+        <Route path="/create-new">
+          <NewCharacterForm />
+        </Route>
+      </Switch>
     </div>
   );
 }
