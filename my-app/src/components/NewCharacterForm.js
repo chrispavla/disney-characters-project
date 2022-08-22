5494c50d1c13823a22f85085c95bce3e0310eab9
Kristina <kristina.hodges7@gmail.com>
2022-08-22 11:56:06 -0400

    formUpdates

--- my-app/src/components/NewCharacterForm.js
+++ my-app/src/components/NewCharacterForm.js
@@ -1,18 +1,47 @@
 import React, { useState } from "react";
 
 function NewCharacterForm() {
+  const [name, setName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [films, setFilms] = useState("");
+  const [isVillain, setIsVillain] = useState(false);
+
   return (
     <div>
       <h2>Add Characters to Disney Universe</h2>
       <form>
         <label for="name">Character name</label>
-        <input type="text" id="name" name="name" placeholder="Name..." />
+        <input
+          required
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <br></br>
         <label for="image">Add image link</label>
-        <input type="text" id="image" name="image" placeholder="Image URL..." />
+        <input
+          required
+          type="text"
+          id="image"
+          name="image"
+          placeholder="Image URL..."
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
         <br></br>
         <label for="film">Add film name</label>
-        <input type="text" name="film" step="0.01" placeholder="Film..." />
+        <input
+          required
+          type="text"
+          name="film"
+          step="0.01"
+          placeholder="Film..."
+          value={films}
+          onChange={(e) => setFilms(e.target.value)}
+        />
         <br></br>
         <label for="villain">Is it a villain?</label>
         <input type="radio" id="villain" name="isVillain" />
