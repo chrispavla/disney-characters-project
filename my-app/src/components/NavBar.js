35186dc81603d7252fca1e4056579bd25b9b61f0
Kristina <kristina.hodges7@gmail.com>
2022-08-22 09:29:43 -0400

    installed bootstrap, modified navbar component

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,22 +1,73 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
-    return (
-        <div id="nav">
-            <NavLink exact to="/">
-                Home
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand">
+          <img
+            src="./291-2917582_mickey-mouse-logo-transparent-hd-png-download.png"
+            alt=""
+            width="30"
+            height="24"
+            className="d-inline-block align-text-top"
+          />
+          Disney Characters
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink className="nav-link" exact to="/" aria-current="page">
+              Home
             </NavLink>
-            <NavLink exact to="/all">
-                All Characters
+            <NavLink className="nav-link" exact to="/all">
+              All Characters
             </NavLink>
-            <NavLink exact to="/favorites">
-                My Favorite Characters
+            <NavLink className="nav-link" exact to="/favorites">
+              My Favorite Characters
             </NavLink>
-            <NavLink exact to="/create-new">
-                Create A New Character 
+            <NavLink
+              className="nav-link"
+              //   class="nav-link"
+              exact
+              to="/create-new"
+            >
+              Create A New Character
             </NavLink>
+          </div>
         </div>
-    )
+      </div>
+    </nav>
+
+    /* <nav class="navbar  bg-light">
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+
+        <NavLink exact to="/all">
+          All Characters
+        </NavLink>
+
+        <NavLink exact to="/favorites">
+          My Favorite Characters
+        </NavLink>
+
+        <NavLink exact to="/create-new">
+          Create A New Character
+        </NavLink>
+      </nav> */
+    // </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
